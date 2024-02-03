21ed0e3aace834e61eb8a3f2bcb4d047f7b18e98
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-02-03 13:46:03 -0600

    added icon button

--- src/components/KeyInput/index.js
+++ src/components/KeyInput/index.js
@@ -4,10 +4,14 @@ import React, { Component } from "react";
 
 import {
   Input,
+  IconButton,
+  InputGroup,
   FormControl,
   FormHelperText,
   FormErrorMessage,
+  InputRightElement,
 } from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 export default class KeyInput extends Component {
   constructor(props) {
@@ -21,6 +25,10 @@ export default class KeyInput extends Component {
     };
   }
 
+  componentDidMount() {
+    this.setState({ ...this.state, helpText: this.props.helpText });
+  }
+
   componentDidUpdate() {
     let { helpText } = this.state;
 
@@ -28,17 +36,38 @@ export default class KeyInput extends Component {
       this.setState({ ...this.state, helpText });
   }
 
-  updateData = (data) => this.setState({ ...this.state, data });
+  updateData = (data) =>
+    this.setState({ ...this.state, data: data.target.value });
+
+  toggleMode = () =>
+    this.setState({
+      ...this.state,
+      mode: this.state.mode === "password" ? "text" : "password",
+    });
 
   render() {
     return (
       <FormControl>
-        <Input
-          type={this.state.mode}
-          placeholder="Key"
-          value={this.state.data}
-          onChange={this.updateData}
-        />
+        <InputGroup size="md">
+          <Input
+            pr="4.5rem"
+            type={this.state.mode}
+            placeholder="Key"
+            value={this.state.data}
+            onChange={this.updateData}
+          />
+          <InputRightElement width="4.5rem">
+            <IconButton
+              h="1.75rem"
+              variant="ghost"
+              colorScheme="gray"
+              onClick={this.toggleMode}
+              icon={
+                this.state.mode === "password" ? <ViewIcon /> : <ViewOffIcon />
+              }
+            />
+          </InputRightElement>
+        </InputGroup>
         {this.state.errorText ? (
           <FormErrorMessage>{this.state.errorText}</FormErrorMessage>
         ) : (
