f0eb230ddcd31510eda20be7105209ef4c0c62ba
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-02-03 14:23:53 -0600

    title component

---
+++ src/components/Title/index.js
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { Component } from "react";
+
+import style from "./style.module.css";
+
+export default class Title extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: "",
+      subtitle: false,
+    };
+  }
+
+  componentDidMount() {
+    let { data, subtitle } = this.state;
+
+    data = this.props.children || this.props.data || "";
+    subtitle = this.props.subtitle || false;
+
+    this.setState({ ...this.state, data, subtitle });
+  }
+
+  componentDidUpdate() {
+    let { subtitle } = this.state;
+
+    if (subtitle !== this.props.subtitle)
+      this.setState({ ...this.state, subtitle: this.props.subtitle });
+  }
+
+  render() {
+    return (
+      <div className={this.state.subtitle ? style.subtitle : style.title}>
+        {this.state.data}
+      </div>
+    );
+  }
+}
