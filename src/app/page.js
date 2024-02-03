df496f32d1d73f4334b653430ca7978f7bd9b530
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-02-03 14:53:13 -0600

    added home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,71 @@
-export default function Home() {
-  return (
-    <>
-      <div>
-        Hello World
-        <br />
-        This is Make It Secret
-        <hr />
-        Work in Progress
-      </div>
-    </>
-  );
+"use client";
+
+import React, { Component } from "react";
+
+import { Button, Box, Center, VStack } from "@chakra-ui/react";
+import Image from "next/image";
+
+import Label from "@/components/Label";
+import Title from "@/components/Title";
+import FileModal from "@/components/FileModal";
+
+import style from "./page.module.css";
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modal: false,
+      file: null,
+    };
+  }
+
+  toggleFileModal = (modal) => this.setState({ ...this.state, modal });
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="center h-100">
+          <VStack spacing={45} align="stretch">
+            <Box>
+              <Center>
+                <Title>Make It Secret</Title>
+              </Center>
+              <br />
+              <Center>
+                <Title subtitle={true}>Encrypt or Decrypt files</Title>
+              </Center>
+            </Box>
+            <Box>
+              <div className={style.uploadArea}>
+                <Image
+                  src="/upload.svg"
+                  height={270}
+                  width={270}
+                  alt="upload"
+                />
+                <Label>Click or Drop files here</Label>
+              </div>
+            </Box>
+            <Box>
+              <Center>
+                <Label>Made by Satshree Shrestha</Label>
+              </Center>
+              <br />
+              <Center>
+                <Button size="sm" variant="solid" colorScheme="blue">
+                  About Make it Secret
+                </Button>
+              </Center>
+            </Box>
+          </VStack>
+        </div>
+        <FileModal
+          open={this.state.modal}
+          closeModal={() => this.toggleFileModal(false)}
+        />
+      </React.Fragment>
+    );
+  }
 }
